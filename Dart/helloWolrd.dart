8c41d1b07c94a73646df7a9ff0209331e654634f
Lia <livcristinna@gmail.com>
2021-11-22 19:58:59 -0300

    type data

--- Dart/helloWolrd.dart
+++ Dart/helloWolrd.dart
@@ -1,4 +1,16 @@
-print('Hello Wolrd in Dart!');
-print('Hello Wolrd in Dart!');
-print('Hello Wolrd in Dart!');
-print('Hello Wolrd in Dart!');
+void main() {
+  print('Hello Wolrd in Dart!');
+  print('Hello Wolrd in Dart!');
+  print('Hello Wolrd in Dart!');
+  print('Hello Wolrd in Dart!');
+}
+
+void tipos() {
+  var a = 1;
+  // a = 'baba';
+
+  print(a);
+
+  // Quando usamos var a primeira atribuição que passa pela variável passa a ser o tipo dela.
+  // Quando vc usa o tempo dynamic a linguagem ignora a troca de tipo de dados dentro da aplicação.
+}
